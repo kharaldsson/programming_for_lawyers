61518d7bfca00aecd2892f2305a52b0d595b0aae
Karl Haraldsson <karl.haraldsson@gmail.com>
2018-02-11 14:10:27 -0600

    Add files via upload
    simple vigenere's cyper

---
+++ vig.py
@@ -0,0 +1,23 @@
+# Vigenere's Cypher Assignment
+# Karl Haraldsson
+# Due Febryary 13, 2018
+
+
+def vig(s,keyword):
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    string = s.upper()
+    keyword = keyword.upper()
+    newstring = ""
+    keyword_index = []
+    for i in range(len(string)):
+        keyword_index.append(alphabet.find(keyword[i % len(keyword)]))
+    for ind in range(len(string)):
+        if string[i] not in alphabet:
+            newstring += string[ind]
+        else:
+                newstring += alphabet[(alphabet.find(string[ind]) + keyword_index[ind]) % 26]
+    return newstring
+
+
+test_vig = vig("abcabc","aaa")
+print(test_vig)
