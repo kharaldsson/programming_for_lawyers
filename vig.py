7aa266d7cc901dbe60dc3f19f55fd3a3058a4e2e
Karl Haraldsson <karl.haraldsson@gmail.com>
2018-02-11 15:33:31 -0600

    Update vig.py

--- vig.py
+++ vig.py
@@ -19,6 +19,30 @@ def vig(s,keyword):
     return newstring
 
 
-# Testing
-test_vig = vig("abcabc","aaa")
-print(test_vig)
+"""
+Create vigenere's cypher using caesar() function. Resubmitted on 11 Feb.
+"""
+
+# Include caesar cypher for use in vig_with_caesar() function
+def caesar(s,k):
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    t = s.upper()
+    newstring = ""
+    for i in range(len(t)):
+        if t[i] not in alphabet:
+            newstring += t[i]
+        else:
+            newstring += alphabet[(alphabet.find(t[i]) + k) % 26]
+    return newstring
+
+
+# Define function vig_with_caesar() function which takes two inputs and returns encrypted text.
+def vig_with_caesar(s, keyword):
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    string = s.upper()
+    keyword = keyword.upper()
+    empty_string = ""
+    # Loop through string and apply caesar() to each letter in string applying the key from keyword
+    for i in range(len(string)):
+        empty_string += str(caesar(string[i], alphabet.find(keyword[i % len(keyword)])))
+    return empty_string
